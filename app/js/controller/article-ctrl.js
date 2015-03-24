522204baac949e9835ceed48ae930223f1a5fa98
Buget Jérémy <jbuget@octo.com>
2015-03-24 18:14:03 +0100

    Initialisation du repository

---
+++ app/js/controller/article-ctrl.js
@@ -0,0 +1,14 @@
+var app = app || {};
+
+app.articleCtrl = (function() {
+    
+    'use strict';
+    
+    function init() {
+        console.log('TODO : app.articleCtrl.init()');
+    }
+    
+    return {
+        init: init
+    };
+})();
---
+++ app/js/controller/articles-ctrl.js
@@ -0,0 +1,61 @@
+var app = app || {};
+
+app.articlesCtrl = (function() {
+    
+    'use strict';
+    
+    // Dependencies
+    var service = null;
+    
+    // Model
+    var articles = [];
+    
+    function loadArticles() {
+        articles = service.getArticles();
+    }
+    
+    function renderArticles() {
+        var $articles = $('.article-list');
+        
+        // on récupère notre item template qui va nous permettre de le cloner pour chaque article
+        var $sample = $('li:first', $articles); 
+        
+        // on supprime la propriété qui le cache (laquelle sert à éviter le clignotement lors du chargement)
+        $sample.removeAttr('style'); 
+        
+        // on supprime l'item template qui a fini de jouer son rôle
+        $sample.remove(); 
+        
+        for (var i = 0; i < articles.length; i++) { 
+            // on récupère l'article à afficher
+            var article = articles[i];
+
+            // on récupère les différents éléments graphiques qu'on va vouloir renseigner
+            var $article = $sample.clone(true);
+            var $articleLink = $('.article-link', $article);
+            var $articleAuthor = $('.article-author', $article);
+            var $articlePublicationDate = $('.article-publication-date', $article);
+            var $articleExcerpt = $('.article-excerpt', $article);
+            
+            // on configure les valeurs des éléments graphiques (titre, auteur, etc.)
+            $articleLink.text("#" + article.id + " - " + article.title);
+            $articleLink.attr("href", "article.html?id=" + article.id);
+            $articleAuthor.text(article.author);
+            $articlePublicationDate.text(article.publicationDate);
+            $articleExcerpt.html(article.excerpt);
+            
+            // on rajoute l'article dans la liste des articles
+            $articles.append($article);
+        }
+    }
+    
+    function init(articleService) {
+        service = articleService;
+        loadArticles();
+        renderArticles();
+    }
+    
+    return {
+        init: init
+    };
+})();
